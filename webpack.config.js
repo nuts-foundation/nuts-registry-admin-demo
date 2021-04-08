22c1500669c414ec8df947b8e41ae1171bb40e33
stevenvegt <steven.vandervegt@nedap.com>
2021-04-08 17:03:44 +0200

    Generate index.html file in dist
    Clean dist before build
    Generate index.html from template

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Nuts Demo registry admin",
+      template: "./web/src/index.html"
+    }),
+  ],
   entry: {
     index: './web/src/index.js',
     print: './web/src/print.js',
@@ -9,5 +16,6 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'web/dist'),
+    clean: true,
   },
 };
