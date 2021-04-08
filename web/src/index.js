7ad0eaad5e09a51ab1707332b27482310990a03c
stevenvegt <steven.vandervegt@nedap.com>
2021-04-08 18:18:54 +0200

    Setup with basic hello Nuts component

--- web/src/index.js
+++ web/src/index.js
@@ -1,16 +1,5 @@
-import printMe from './print.js';
+import * as Vue from 'vue'
+import App from './App.vue'
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = "hello webpack";
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-  return element;
-}
-
-document.body.appendChild(component());
+const app = Vue.createApp(App)
+const vm = app.mount('#app')
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
+
 
 module.exports = {
   mode: "development",
   plugins: [
+    new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
       title: "Nuts Demo registry admin",
       template: "./web/src/index.html"
@@ -19,4 +22,19 @@ module.exports = {
     path: path.resolve(__dirname, 'web/dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
 };
