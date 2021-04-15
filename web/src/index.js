713209a80fb0f359298a270a700c7f0c8482972e
stevenvegt <steven.vandervegt@nedap.com>
2021-04-15 11:48:43 +0200

    Add empty service provider page

--- web/src/index.js
+++ web/src/index.js
@@ -8,7 +8,9 @@ import Landing from './Landing.vue'
 import PublicMenu from './layout/PublicMenu.vue'
 import Login from './Login.vue'
 import Logout from './Logout.vue'
+import NotFound from './NotFound.vue'
 import Customers from './admin/Customers.vue'
+import ServiceProvider from './admin/ServiceProvider.vue'
 
 const routes = [
   {path: '/', components: {default: Landing, menu: PublicMenu}},
@@ -20,12 +22,23 @@ const routes = [
     children: [
       {
         path: '',
+        name: 'admin-home',
+        redirect: '/admin/customers'
+      },
+      {
+        path: 'customers',
+        name: 'customers',
         component: Customers
+      },
+      {
+        path: 'service-provider',
+        name: 'service-provider',
+        component: ServiceProvider
       }
     ],
     meta: {requiresAuth: true}
-
-  }
+  },
+  {path: '/:pathMatch*', name: 'NotFound', components: {default: NotFound, menu: PublicMenu}},
 ]
 
 const router = createRouter({
