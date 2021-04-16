14ff2f9b16c0dd690fb7f57001307bb99d8296d5
stevenvegt <steven.vandervegt@nedap.com>
2021-04-16 22:09:26 +0200

    Implements roughly the new UI layout in Admin

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="App min-w-screen min-h-screen flex flex-col bg-gray-100 font-sans">
-    <nrad-header></nrad-header>
-
-    <div class="content-wrapper pt-4 flex-1 bg-grey-lighter">
-      <div class="content py-4 container max-w-6xl mx-auto bg-white m-4 p-6 rounded-md shadow-md">
-        <router-view/>
-      </div>
-    </div>
-
-    <nrad-footer></nrad-footer>
-  </div>
+  <router-view/>
 </template>
 
 <script>
