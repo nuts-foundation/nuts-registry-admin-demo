bafca67f31ed071a2a4f216b259b1b92a31bfe90
stevenvegt <steven.vandervegt@nedap.com>
2021-05-10 17:40:43 +0200

    Add stub ManageVCs page

--- web/src/admin/AdminApp.vue
+++ web/src/admin/AdminApp.vue
@@ -28,14 +28,14 @@
           You as a Service Provider
         </router-link>
         <router-link
-            to=""
+            :to="{name: 'admin.manageVCs'}"
             class="menu-link">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24"
                stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>
           </svg>
-          Trusted Credential Issuers
+          Manage Credential Issuers
         </router-link>
         <router-link
             to="/logout"
@@ -59,6 +59,7 @@
 
 <script>
 import StatusBar from "../components/StatusBar.vue";
+
 export default {
   components: {StatusBar},
   data() {
---
+++ web/src/admin/ManageVCs.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Manage Verifiable Credentials</h1>
+  <div v-for="(issuers, type) in credentialIssuers">
+    <span>Type: {{type}}</span>
+    <div>
+      <h1>Trusted Issuers:</h1>
+      <ul v-for="issuer in issuers.trusted">
+        <li>{{issuer.did}}{{issuer.name}}</li>
+      </ul>
+    </div>
+    <div>
+      <h1>Untrusted Issuers:</h1>
+      <ul v-for="issuer in issuers.untrusted">
+        <li>{{issuer.did}}{{issuer.name}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  emits: ['statusUpdate'],
+  data() {
+    return {
+      credentialIssuers: {
+        'NutsOrganizationCredential': {trusted: [{did: "did:nuts:123", name:"CareSoft NV"}, {did: "did:nuts:456", name:"Should we Care?"}], untrusted: [{did: "did:nuts:666", name: "EvilSoft"}]},
+        'LRZACredential': {trusted: [{did: "did:web:lrza.nl", name: "LRZa"}], untrusted: [{did: "did:nuts:666", name: "EvilSoft"}]}
+      }
+    }
+  },
+  methods: {
+    fetchTrusted(credentialType) {
+
+    },
+    fetchUntrusted(credentialType) {
+
+    },
+    trustIssuer(credentialType, issuerID) {
+
+    },
+    untrustIssuer(credentialType, issuerID) {
+
+    }
+  }
+}
+</script>
