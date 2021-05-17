2619e03da3a1e97ab0677a9e7f3e96f0c67b9c15
stevenvegt <steven.vandervegt@nedap.com>
2021-05-17 09:25:32 +0200

    Add empty, loading and error feedback

--- web/src/admin/ManageVCs.vue
+++ web/src/admin/ManageVCs.vue
@@ -3,13 +3,16 @@
     <h1 class="text-3xl">Manage Verifiable Credentials</h1>
   </div>
   <div class="border rounded-md">
+    <p v-if="!!fetchError" class="m-4">Could not fetch credential issuers: {{ fetchError }}</p>
+    <div class="m-4" v-if="loading">Loading...</div>
+    <div class="m-4" v-if="!loading && Object.keys(credentialIssuers).length === 0 && !fetchError">No credential issuers yet.</div>
     <div v-for="(issuers, type) in credentialIssuers">
       <div class="bg-gray-200 p-2">{{ type }}</div>
 
       <ul v-for="issuer in issuers">
         <li class="flex justify-between p-2">
           <span>{{ issuer.serviceProvider.id }} - {{ issuer.serviceProvider.name }}</span>
-          <form-checkbox  v-model="issuer.trusted" @update:modelValue="toggleTrust(type, issuer)">Trusted</form-checkbox>
+          <form-checkbox v-model="issuer.trusted" @update:modelValue="toggleTrust(type, issuer)">Trusted</form-checkbox>
         </li>
       </ul>
 
@@ -24,7 +27,9 @@ export default {
   emits: ['statusUpdate'],
   data() {
     return {
-      credentialIssuers: {}
+      credentialIssuers: {},
+      loading: true,
+      fetchError: ""
     }
   },
   methods: {
@@ -35,14 +40,16 @@ export default {
           })
           .catch((reason) => {
             console.log("fetch failed: ", reason)
+            this.fetchError = reason
           })
+          .finally(() => this.loading = false)
     },
     toggleTrust(type, issuer) {
       console.log("toggle", type, issuer)
       this.$api.put(`web/private/credential/${type}/issuer/${encodeURIComponent(issuer.serviceProvider.id)}`, {trusted: issuer.trusted})
-      .then((response)=> {
-      }).catch((reason => console.log("update status failed:", reason)))
-      .finally(this.fetchIssuers)
+          .then((response) => {
+          }).catch((reason => console.log("update status failed:", reason)))
+          .finally(this.fetchIssuers)
     },
   },
   mounted() {
