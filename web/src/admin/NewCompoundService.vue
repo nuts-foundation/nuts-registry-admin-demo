4497df0f462f524487c2151ec6df5316d038ea14
stevenvegt <steven.vandervegt@nedap.com>
2021-05-27 20:52:43 +0200

    Delete endpoint

--- web/src/admin/NewCompoundService.vue
+++ web/src/admin/NewCompoundService.vue
@@ -3,7 +3,7 @@
                 title="Register new Service" type="add">
 
     <p class="mb-3 text-sm">
-      Here you can compose a new Service from endpoints. A service can be than be enabled per customer.
+      Here you can compose a new Service from endpoints. A service can than be enabled per customer.
     </p>
 
     <p v-if="apiError" class="p-3 bg-red-100 rounded-md">Could not register service: {{ apiError }}</p>
@@ -20,15 +20,44 @@
         <label for="endpointTypeInput">Name:</label>
         <input type="text" v-model="service.name" id="endpointTypeInput" required>
       </div>
-      <div v-for="(endpointRef, type) in service.endpoints">
-        {{ type }}:{{ endpointRef }}
-      </div>
-      <input type="text" placeholder="type" v-model="newEndpointType">:
-      <select name="" id="" class="form-select" v-model="selectedEndpoint">
-        <option defaul selected value="">Select an endpoint</option>
-        <option v-for="(endpoint, endpointId) in availableEndpoints" :value="endpointId">{{ endpoint.type }}</option>
-      </select>
-      <button class="btn-primary" @click="addEndpoint(newEndpointType, selectedEndpoint)">Add endpoint</button>
+      <label>Endpoints:
+        <table>
+          <tr>
+            <th>Type</th>
+            <th colspan="2">Endpoint name</th>
+          </tr>
+          <tr v-for="(endpointRef, name) in service.endpoints">
+            <td>{{ name }}</td>
+            <td>{{ endpointRef.split('=')[1] }}</td>
+            <td>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 hover:text-gray-500"
+                   @click="deleteEndpoint(name, endpointRef)" fill="none" viewBox="0 0 24 24"
+                   stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+              </svg>
+            </td>
+          </tr>
+          <tr v-if="Object.keys(availableEndpoints).length" >
+            <td>
+              <input type="text" placeholder="type" v-model="newEndpointType">
+            </td>
+            <td>
+              <select :disabled="Object.keys(availableEndpoints).length === 0" name="" id="" class="form-select"
+                      v-model="selectedEndpoint">
+                <option selected value="">Select an endpoint</option>
+                <option v-for="(endpoint, endpointId) in availableEndpoints" :value="endpointId">{{
+                    endpoint.type
+                  }}
+                </option>
+              </select>
+            </td>
+            <td>
+              <button class="btn-primary" @click="addEndpoint(newEndpointType, selectedEndpoint)">Add</button>
+            </td>
+          </tr>
+        </table>
+      </label>
     </form>
 
   </modal-window>
@@ -77,6 +106,9 @@ export default {
           })
     },
     addEndpoint(type, endpointID) {
+      if (!type) {
+        return
+      }
       let endpoint = this.allEndpoints[endpointID]
       if (!endpoint) {
         return
@@ -87,6 +119,15 @@ export default {
       this.selectedEndpoint = ""
       this.newEndpointType = ""
       delete this.availableEndpoints[endpointID]
+    },
+    deleteEndpoint(name, ref) {
+      let type = ref.split("=")[1]
+      let endpoint = Object.values(this.allEndpoints).find((el) => el.type == type)
+      if (!endpoint) {
+        return
+      }
+      delete this.service.endpoints[name]
+      this.availableEndpoints[endpoint.id] = endpoint
     },
     checkForm(e) {
       // reset the errors
