541848b413eaa7efb1af40bcf584b52885f5b8db
stevenvegt <steven.vandervegt@nedap.com>
2021-12-01 16:51:38 +0100

    Single root node in template

--- web/src/admin/Customers.vue
+++ web/src/admin/Customers.vue
@@ -1,44 +1,47 @@
 <template>
-  <div class="flex justify-between mb-6">
-    <h1>Your care organizations</h1>
+  <div>
 
-    <button
-        @click="$router.push({name: 'admin.newCustomer'})"
-        class="float-right inline-flex items-center bg-nuts w-10 h-10 rounded-lg justify-center shadow-md"
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff">
-        <path d="M0 0h24v24H0V0z" fill="none"/>
-        <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
-      </svg>
-    </button>
-  </div>
+    <div class="flex justify-between mb-6">
+      <h1>Your care organizations</h1>
 
-  <div class="mt-8 bg-white p-5 shadow-lg rounded-lg">
-    <p v-if="fetchError" class="m-4">Could not fetch customers: {{ fetchError }}</p>
-    <div class="m-4" v-if="loading">Loading...</div>
-    <div class="m-4" v-if="!loading && customers.length == 0 && !fetchError">No customers yet, add one!</div>
-    <table v-if="customers.length > 0" class="min-w-full divide-y divide-gray-200">
-      <thead>
-      <tr>
-        <th class="thead">Customer ID</th>
-        <th class="thead">Name</th>
-        <th class="thead">City</th>
-        <th class="thead">Published</th>
-      </tr>
-      </thead>
-      <tbody class="tbody">
-      <tr class="hover:bg-gray-100 cursor-pointer"
-          @click="$router.push({name: 'admin.editCustomer', params: {id: customer.id} })"
-          v-for="{id, name, city, active} in customers" :key="id">
-        <td class="tcell">{{ id }}</td>
-        <td class="tcell">{{ name }}</td>
-        <td class="tcell">{{ city }}</td>
-        <td class="tcell">{{ active }}</td>
-      </tr>
-      </tbody>
-    </table>
+      <button
+          @click="$router.push({name: 'admin.newCustomer'})"
+          class="float-right inline-flex items-center bg-nuts w-10 h-10 rounded-lg justify-center shadow-md"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff">
+          <path d="M0 0h24v24H0V0z" fill="none"/>
+          <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
+        </svg>
+      </button>
+    </div>
+
+    <div class="mt-8 bg-white p-5 shadow-lg rounded-lg">
+      <p v-if="fetchError" class="m-4">Could not fetch customers: {{ fetchError }}</p>
+      <div class="m-4" v-if="loading">Loading...</div>
+      <div class="m-4" v-if="!loading && customers.length == 0 && !fetchError">No customers yet, add one!</div>
+      <table v-if="customers.length > 0" class="min-w-full divide-y divide-gray-200">
+        <thead>
+        <tr>
+          <th class="thead">Customer ID</th>
+          <th class="thead">Name</th>
+          <th class="thead">City</th>
+          <th class="thead">Published</th>
+        </tr>
+        </thead>
+        <tbody class="tbody">
+        <tr class="hover:bg-gray-100 cursor-pointer"
+            @click="$router.push({name: 'admin.editCustomer', params: {id: customer.id} })"
+            v-for="{id, name, city, active} in customers" :key="id">
+          <td class="tcell">{{ id }}</td>
+          <td class="tcell">{{ name }}</td>
+          <td class="tcell">{{ city }}</td>
+          <td class="tcell">{{ active }}</td>
+        </tr>
+        </tbody>
+      </table>
+      <router-view name="modal" @statusUpdate="updateStatus"></router-view>
+    </div>
   </div>
-  <router-view name="modal" @statusUpdate="updateStatus"></router-view>
 </template>
 
 <script>
--- web/src/admin/ManageVCs.vue
+++ web/src/admin/ManageVCs.vue
@@ -1,18 +1,23 @@
 <template>
-  <h1>Manage Verifiable Credentials</h1>
+  <div>
+    <h1>Manage Verifiable Credentials</h1>
 
-  <div class="mt-8 bg-white p-5 shadow-lg rounded-lg">
-    <p v-if="!!fetchError" class="m-4">Could not fetch credential issuers: {{ fetchError }}</p>
-    <div class="m-4" v-if="loading">Loading...</div>
-    <div class="m-4" v-if="!loading && Object.keys(credentialIssuers).length === 0 && !fetchError">No credential issuers yet.</div>
-    <div v-for="(issuers, type) in credentialIssuers" :key="type">
-      <div class="font-medium p-2">{{ type }}</div>
-      <ul v-for="issuer in issuers" :key="issuer.serviceProvider.id">
-        <li class="flex justify-between p-2">
-          <span>{{ issuer.serviceProvider.id }} - {{ issuer.serviceProvider.name }}</span>
-          <form-checkbox v-model="issuer.trusted" @update:modelValue="toggleTrust(type, issuer)">Trusted</form-checkbox>
-        </li>
-      </ul>
+    <div class="mt-8 bg-white p-5 shadow-lg rounded-lg">
+      <p v-if="!!fetchError" class="m-4">Could not fetch credential issuers: {{ fetchError }}</p>
+      <div class="m-4" v-if="loading">Loading...</div>
+      <div class="m-4" v-if="!loading && Object.keys(credentialIssuers).length === 0 && !fetchError">No credential
+        issuers yet.
+      </div>
+      <div v-for="(issuers, type) in credentialIssuers" :key="type">
+        <div class="font-medium p-2">{{ type }}</div>
+        <ul v-for="issuer in issuers" :key="issuer.serviceProvider.id">
+          <li class="flex justify-between p-2">
+            <span>{{ issuer.serviceProvider.id }} - {{ issuer.serviceProvider.name }}</span>
+            <form-checkbox v-model="issuer.trusted" @update:modelValue="toggleTrust(type, issuer)">Trusted
+            </form-checkbox>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
