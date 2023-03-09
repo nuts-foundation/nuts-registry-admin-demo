d76a983a94e4c7cd6275ae92a9b8464a0e7fc4b1
Wout Slakhorst <wout.slakhorst@nedap.com>
2023-03-09 09:47:34 +0100

    update deprecated tailwind option

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./web/src/index.html', './web/src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./web/src/index.html', './web/src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
